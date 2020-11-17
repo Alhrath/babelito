da4b443b8cba1f26f49263abaf87def8af156239
slipihollow <32129616+slipihollow@users.noreply.github.com>
2020-11-17 17:47:53 +0100

    added jump touch differenciation

--- Babelito/src/engine/singleton/globals.gd
+++ Babelito/src/engine/singleton/globals.gd
@@ -2,11 +2,11 @@ extends Node
 
 var UP = Vector2(0,-1)
 
-var GRAVITY = 15
+var GRAVITY = 900
 var MAXFALLSPEED = 1500
 
-var JUMP_FORCE = 640
-var ACCELERATION = 175
-var DECELERATION = 0.1
+var JUMP_FORCE = 666
+var ACCELERATION = 325
+var DECELERATION = 0.2
 
 var AI_WAIT_TIME = 1.0
--- Babelito/src/entity/player/player.gd
+++ Babelito/src/entity/player/player.gd
@@ -1,12 +1,16 @@
 extends "../entity.gd"
 
 func _input(event):
-	if is_on_floor () and event.is_action_pressed("ui_up"):
+	if is_on_floor () and event.is_action_pressed("jump"):
 		jump()
 
 func _physics_process(delta):
 	if  Input.is_action_pressed("ui_left"):
 		move_left()
+	
+	#permet de donner des différences de saut variable (si appuyé longtemps fait une certaine hauteur, si tap saute à une hauteur plus courte)
+	if Input.is_action_just_released("jump") && motion.y < 0 :
+		motion.y = 0
 	
 	elif Input.is_action_pressed("ui_right"):
 		move_right()
