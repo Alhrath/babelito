2387451388e75992c513ab3811491a43b420b28d
TheCaptainKiwi <56768066+TheCaptainKiwi@users.noreply.github.com>
2020-12-08 15:26:58 +0100

    Addition of crouch/crawling
    adventures of the crouch key ("down") and the mysterious smiley

--- Babelito/src/entity/player/PlayerStatemachine.gd
+++ Babelito/src/entity/player/PlayerStatemachine.gd
@@ -1,5 +1,7 @@
 extends "res://src/engine/singleton/StateMachine.gd"
 
+const STOP_THRESHOLD = 32.0
+
 func _ready():
 
 	add_state("idle")
@@ -9,35 +11,43 @@ func _ready():
 	add_state("throw_godot")
 	add_state("wall_slide")
 	add_state("dead")
+	add_state("crouch")
+	add_state("crawl")
 	call_deferred("set_state", states.idle) #defining "idle" as default
 
 func _state_logic(delta):
 	parent._update_move_direction()
 	parent._update_wall_direction()
 	
-	if state != states.wall_slide:
-		parent._handle_move_input()
+	if [states.idle, states.run, states.jump, states.fall].has(state):
+		parent._handle_movement()
+		#to set which states can move
 		
-	parent._apply_gravity(delta)
+	elif [states.crouch, states.crawl].has(state):
+		parent._handle_movement(parent.CRAWL_SPEED)
+	#to set different speed when crawling
 	
 	if state == states.wall_slide:
 		parent._cap_gravity_wall_slide()
 		parent._handle_wall_slide_sticking()
+		
+	parent._apply_gravity(delta)	
 	parent._apply_movement()
 	
-func _input(event): 
-	if state == states.idle || state == states.run:
+func _input(event): # == which state can do what 
+	if [states.idle, states.run, states.crouch, states.crawl].has(state) && parent.can_stand():
+	#for all these states:
 		if event.is_action_pressed("throw"):
 			set_state(states.throw_godot)
-		
-		if event.is_action_pressed("jump"): 
-		
+			
+		elif event.is_action_pressed("jump"): 
 			if Input.is_action_pressed("down"):
+			#when jump + down are pressed ==> drop through platform
 				if parent._check_is_grounded(parent.drop_thru_raycasts):
 					parent.set_collision_mask_bit(parent.DROP_THRU_BIT, false)
 			else:
-				parent.velocity.y = Globals.max_jump_velocity
-				parent.is_jumping = true
+				parent.jump()
+
 				
 	elif state == states.wall_slide:
 		if event.is_action_pressed("jump"):
@@ -45,8 +55,9 @@ func _input(event):
 			set_state(states.jump)
 
 	elif state == states.jump:
-		if event.is_action_released("jump") && parent.velocity.y < Globals.min_jump_velocity:
-			parent.velocity.y = Globals.min_jump_velocity
+		if event.is_action_released("jump"):
+			parent.variable_jump()#variation in jump intensity
+			
 		if event.is_action_pressed("throw"):
 			set_state(states.throw_godot)
 			
@@ -61,7 +72,9 @@ func _get_transition(delta):
 					return states.fall
 			elif parent.velocity.x != 0:
 				return states.run
-				
+			elif Input.is_action_pressed("down"):
+				return states.crouch
+			
 		states.run:
 			if !parent.is_on_floor():
 				if parent.velocity.y < 0:
@@ -70,7 +83,9 @@ func _get_transition(delta):
 					return states.fall
 			elif parent.velocity.x == 0:
 				return states.idle
-		
+			elif Input.is_action_pressed("down"):
+				return states.crawl
+				
 		states.jump:
 			if parent.wall_direction != 0 && parent.wall_slide_cooldown.is_stopped():
 				return states.wall_slide
@@ -108,7 +123,29 @@ func _get_transition(delta):
 				return states.idle
 			elif parent.wall_direction == 0:
 				return states.fall
-
+				
+		states.crouch:
+			if !Input.is_action_pressed("down") && parent.can_stand():
+				return states.idle
+			elif !parent.is_on_floor():
+				if parent.velocity.y < 0:
+					return states.jump
+				else:
+					return states.fall
+			elif abs(parent.velocity.x) >= STOP_THRESHOLD:
+				return states.crawl
+				
+		states.crawl:
+			if !Input.is_action_pressed("down") && parent.can_stand():
+				return states.run
+			elif !parent.is_on_floor():
+				if parent.velocity.y < 0:
+					return states.jump
+				else:
+					return states.fall
+			elif abs(parent.velocity.x) < STOP_THRESHOLD:
+				return states.crouch
+				
 	return null
 
 func _enter_state(new_state, old_state):
@@ -135,12 +172,31 @@ func _enter_state(new_state, old_state):
 			parent.anim_player.play("wall_slide")
 			parent.body.scale.x = -parent.wall_direction
 			
-
+		states.crouch:
+			parent.anim_player.play("crouch")
+			if old_state != states.crawl:
+				parent._on_crouch()
+		states.crawl:
+			parent.anim_player.play("crawl")
+			if old_state != states.crouch:
+				parent._on_crouch()
+		
 func _exit_state(old_state, new_state):
 	match old_state:
 		states.wall_slide:
 			parent.wall_slide_cooldown.start()
-			
+
+		states.crouch:
+			if new_state != states.crawl:
+				parent._on_stand()
+		
+		states.crawl:
+			if new_state != states.crouch:
+				parent._on_stand()
+
+func is_crouched():
+	return [states.crouch, states.crawl].has(state)
+
 func _on_WallSlideStickyTimer_timeout():
 	if state == states.wall_slide:
 		set_state(states.fall)	
--- Babelito/src/entity/player/player.gd
+++ Babelito/src/entity/player/player.gd
@@ -6,8 +6,7 @@ signal health_updated(health)
 signal killed()
 
 const GodotProjectile_PS = preload("res://src/entity/weapons/GodotProjectile.tscn")
-
-
+const CRAWL_SPEED = 32 * 5 #=speed when crawling
 const SLOPE_STOP_THRESHOLD = 64 #get better slope movements
 const DROP_THRU_BIT = 1 # collision variable for dropping through platform
 var WALL_JUMP_VELOCITY = Vector2(1,-2) * Globals.move_speed *1.5
@@ -30,6 +29,12 @@ var is_grounded = false#to permit a check_ground method associated with raycasts
 var is_wall_sliding = false
 var wall_direction = 1
 
+
+onready var state_machine = $StateMachine
+onready var standing_collision = $StandingShape
+onready var crouching_collision = $CrouchingShape
+onready var standing_hitbox = $Hitbox/StandingShape
+onready var crouching_hitbox = $Hitbox/CrouchingShape
 onready var left_wall_raycasts = $WallRaycasts/LeftWallRaycasts
 onready var right_wall_raycasts = $WallRaycasts/RightWallRaycasts
 onready var wall_slide_cooldown = $WallSlideCooldown
@@ -53,7 +58,8 @@ func _apply_gravity(delta):
 		_cap_gravity_wall_slide()
 
 func _update_move_direction():
-	move_direction = -int(Input.is_action_pressed("move_left")) + int(Input.is_action_pressed("move_right")) #inputs for movement
+	move_direction = -int(Input.is_action_pressed("move_left")) + int(Input.is_action_pressed("move_right")) 
+#inputs for movement
 	
 func _cap_gravity_wall_slide():
 	var max_velocity = 96 *2 if !Input.is_action_pressed("down") else 8*96 
@@ -79,8 +85,9 @@ func _apply_movement():
 	if was_grounded == null || is_grounded != was_grounded:
 		emit_signal("grounded_updated", is_grounded)
 	
-func _handle_move_input():
-	velocity.x = lerp(velocity.x, move_speed * move_direction, _get_h_weight())
+func _handle_movement(var move_speed = self.move_speed): ####with in (var move_speed = self.move_speed)
+	var move_input_speed = -Input.get_action_strength("move_left") + Input.get_action_strength("move_right")
+	velocity.x = lerp(velocity.x, move_speed * move_input_speed, _get_h_weight())
 	if move_direction != 0:
 		$Body.scale.x = move_direction
 		
@@ -89,7 +96,7 @@ func _handle_move_input():
 	elif Input.is_action_pressed("move_right") or velocity.x > 0:
 		Globals.playerfacing = 1
 
-func _handle_wall_slide_sticking():
+func _handle_wall_slide_sticking(): #timer to control the stick to walls
 	if move_direction != 0 && move_direction != wall_direction:
 		if wall_slide_sticky_timer.is_stopped():
 			wall_slide_sticky_timer.start()
@@ -102,20 +109,20 @@ func _get_h_weight():
 	else:
 		if move_direction == 0:
 			return 0.02
-		elif move_direction == sign(velocity.x) && abs(velocity.x) > Globals.move_speed: 
+		elif move_direction == sign(velocity.x) && abs(velocity.x) > move_speed: 
 			return 0.0 
 		else:
 			return 0.1
 
-func _check_is_grounded(raycasts= self.raycasts):
+func _check_is_grounded(raycasts = self.raycasts):
 	for raycast in raycasts.get_children():
 		if raycast.is_colliding():
 			return true	
-	
+#Use of raycasts to detect if the player touches the ground
 	return false
 
 func jump():
-	Globals.velocity.y = Globals.max_jump_velocity 
+	velocity.y = Globals.max_jump_velocity 
 	is_jumping = true
 	
 func wall_jump():
@@ -130,7 +137,7 @@ func variable_jump():
 func _update_wall_direction():
 	var is_near_wall_left = _check_is_valid_wall(left_wall_raycasts)
 	var is_near_wall_right = _check_is_valid_wall(right_wall_raycasts)
-	
+#Determine the direction of the detected wall
 	if is_near_wall_left && is_near_wall_right:
 		wall_direction = move_direction
 	else:
@@ -142,21 +149,25 @@ func _check_is_valid_wall(wall_raycasts):
 			var dot = acos(Vector2.UP.dot(raycast.get_collision_normal()))
 			if dot > PI * 0.35 && dot < PI * 0.55:
 				return true
+#Use to identify if there's walls
 	return false
 
 func spawn_godot():
+#Function to spawn items to throw
 	if held_item == null:
 		held_item = GodotProjectile_PS.instance()
 		held_item_position.add_child(held_item)
 		
 func _throw_held_item():
+#Function to throw spawned items
 	held_item.launch(Globals.playerfacing)
 	held_item = null
 
 func _on_Area2D_body_exited(body):
 	set_collision_mask_bit(DROP_THRU_BIT, true)
-
+#to let the player pass through == drop-through platform
 func damage(damage_amount):
+#Damage function controlled by invulnerability timer
 	if invulnerability_timer.is_stopped():
 		invulnerability_timer.start()
 		_set_health(health - damage_amount)
@@ -167,6 +178,7 @@ func kill():
 	print("KILLED")
 	
 func _set_health(value):
+#Effective function for changing health
 	var prev_health = health
 	health = clamp(value, 0, max_health)
 	if health != prev_health:
@@ -177,4 +189,42 @@ func _set_health(value):
 
 func _on_InvulnerabilityTimer_timeout():
 	effects_animation.play("playerrest")
+#to see visually when the invulnerability timer is off
+
+func _on_crouch():
+#Change collision when crouching
+	standing_collision.disabled = true
+	crouching_collision.disabled = false
+	standing_hitbox.disabled = true
+	crouching_hitbox.disabled = false
+
+func _on_stand():
+#Change collision when getting back to idle
+	standing_hitbox.disabled = false
+	crouching_hitbox.disabled = true
+	
+	while standing_collision.disabled && !state_machine.is_crouched():
+		if can_stand():
+			standing_collision.disabled = false
+			crouching_collision.disabled = true
+		yield(get_tree(), "physics_frame")
+		
+func can_stand() -> bool: 
+#to check if the character can stand or not where he's
+	var space_state = get_world_2d().direct_space_state
+	var query = Physics2DShapeQueryParameters.new()
+	query.set_shape(standing_collision.shape)
+	query.transform = standing_collision.global_transform
+	query.collision_layer = collision_mask
+	var results = space_state.intersect_shape(query)
+	for i in range(results.size() - 1, -1, -1):
+		var collider = results[i].collider
+		var shape = results[i].shape
+		if collider is CollisionObject2D && collider.is_shape_owner_one_way_collision_enabled(shape):
+			results.remove(i)
+		elif collider is TileMap:
+			var tile_id = collider.get_cellv(results[i].metadata)
+			if collider.tile_set.tile_get_shape_one_way(tile_id, 0):
+				results.remove(i)
+	return results.size() == 0
 
